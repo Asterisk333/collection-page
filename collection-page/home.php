09a1bb39bdfd040e89deb772f8bf5b4b34490072
Astersik <spk02@bluewin.ch>
2022-05-12 11:40:23 +0200

    ---General--- creation if webpage including head,header and footer

---
+++ collection-page/home.php
@@ -0,0 +1,12 @@
+<html lang="de">
+<?php
+    include "./inc/head.inc.php";
+    include "./inc/header.inc.php";
+?>
+    <body>
+
+    </body>
+<?php
+    include "./inc/footer.inc.php";
+?>
+</html>
--- collection-page/inc/head.inc.php
+++ collection-page/inc/head.inc.php
@@ -1,1 +1,11 @@
 <?php
+/*Display errors*/
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+?>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="Refresh" content="30">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
